ee55315c62310482d76f666c097f724b0caf5ee7
Camilo Almeida <camiloalmeida22@gmail.com>
2024-10-14 17:53:53 -0300

    Add constructor to map ExchangeRateDTO to ExchangeRate

--- src/systemnegro/currencyconverter/model/ExchangeRate.java
+++ src/systemnegro/currencyconverter/model/ExchangeRate.java
@@ -1,8 +1,15 @@
 package systemnegro.currencyconverter.model;
 
+import systemnegro.currencyconverter.model.dto.ExchangeRateDTO;
+
 public class ExchangeRate {
     private double rate;
     private double convertedAmount;
+
+    public ExchangeRate(ExchangeRateDTO exchangeRateDTO) {
+        this.rate = exchangeRateDTO.conversion_rate();
+        this.convertedAmount = exchangeRateDTO.conversion_result();
+    }
 
     public double getRate() {
         return rate;
