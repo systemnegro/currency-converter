ea3f98bb0cafe65da8d93483e3ec2659afbd4543
Camilo Almeida <camiloalmeida22@gmail.com>
2024-10-16 16:56:28 -0300

    Refactor ExchangeRate class to include baseCurrency and targetCurrency attributes, and map data from ExchangeRateDTO

--- src/systemnegro/currencyconverter/model/ExchangeRate.java
+++ src/systemnegro/currencyconverter/model/ExchangeRate.java
@@ -5,10 +5,22 @@ import systemnegro.currencyconverter.model.dto.ExchangeRateDTO;
 public class ExchangeRate {
     private double rate;
     private double convertedAmount;
+    private String baseCurrency;
+    private String  targetCurrency;
 
     public ExchangeRate(ExchangeRateDTO exchangeRateDTO) {
         this.rate = exchangeRateDTO.conversion_rate();
         this.convertedAmount = exchangeRateDTO.conversion_result();
+        this.baseCurrency = exchangeRateDTO.base_code();
+        this.targetCurrency = exchangeRateDTO.target_code();
+    }
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public String getTargetCurrency() {
+        return targetCurrency;
     }
 
     public double getRate() {
@@ -24,6 +36,8 @@ public class ExchangeRate {
         return "ExchangeRate{" +
                 "rate=" + rate +
                 ", convertedAmount=" + convertedAmount +
+                ", baseCurrency='" + baseCurrency + '\'' +
+                ", targetCurrency='" + targetCurrency + '\'' +
                 '}';
     }
 }
--- src/systemnegro/currencyconverter/model/dto/ExchangeRateDTO.java
+++ src/systemnegro/currencyconverter/model/dto/ExchangeRateDTO.java
@@ -1,4 +1,4 @@
 package systemnegro.currencyconverter.model.dto;
 
-public record ExchangeRateDTO(double conversion_rate, double conversion_result) {
+public record ExchangeRateDTO(double conversion_rate, double conversion_result, String base_code, String target_code) {
 }
